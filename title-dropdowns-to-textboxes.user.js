edcfe401fec97c71d01c3df04923e6b303522df7
Sophie Hamilton <git-commits@theblob.org>
2014-06-22 15:02:04 +0100

    Fix indentation
    Whoops.

--- title-dropdowns-to-textboxes.user.js
+++ title-dropdowns-to-textboxes.user.js
@@ -21,84 +21,84 @@
   for (var i = 0; i < dropdowns.length; i++) {
     var dropdown = dropdowns.item(i);
     // analyse the dropdown
-	var options = dropdown.options;
-	var values = [];
-	var foundtitles = 0;
-	for (var j = 0; j < options.length; j++) {
-	  var opt = options.item(j);
-	  values.push(opt.value);
-	  var moddedval = opt.value.replace(/\.$/, "");
-	  var moddedtext = opt.textContent.replace(/\.$/, "");
-	  if ((commontitles.indexOf(moddedval) >= 0) && ((moddedtext == moddedval))) {
-	    // found a match
-		foundtitles++;
-	  }
-	}
-	if (foundtitles >= titlethreshold) {
-	  // looks like a compatible title dropdown
-	  var newDiv = document.createElement("div");
-//	  newDiv.appendChild(newTextBox);
+    var options = dropdown.options;
+    var values = [];
+    var foundtitles = 0;
+    for (var j = 0; j < options.length; j++) {
+      var opt = options.item(j);
+      values.push(opt.value);
+      var moddedval = opt.value.replace(/\.$/, "");
+      var moddedtext = opt.textContent.replace(/\.$/, "");
+      if ((commontitles.indexOf(moddedval) >= 0) && ((moddedtext == moddedval))) {
+        // found a match
+        foundtitles++;
+      }
+    }
+    if (foundtitles >= titlethreshold) {
+      // looks like a compatible title dropdown
+      var newDiv = document.createElement("div");
+//      newDiv.appendChild(newTextBox);
       newDiv.innerHTML = "<small>This title dropdown looks textbox-compatible.</small><br>";
-	  newDiv.style.fontWeight = "bold";
-	  var link = document.createElement("a");
-	  link.innerHTML = "Replace this dropdown with a textbox.";
-	  link.href = "#";
-	  link.setAttribute("X-Detected-Values", "\"" + values.join("\", \"") + "\"");
-	  link.addEventListener("DOMActivate", function(e) {
-		var me = e.target;
-	    var mydiv = me.parentNode;
-		var dropdown = mydiv.previousSibling;
-		var titles = me.getAttribute("X-Detected-Values");
-		var selected = dropdown.options.item(dropdown.selectedIndex).value;
+      newDiv.style.fontWeight = "bold";
+      var link = document.createElement("a");
+      link.innerHTML = "Replace this dropdown with a textbox.";
+      link.href = "#";
+      link.setAttribute("X-Detected-Values", "\"" + values.join("\", \"") + "\"");
+      link.addEventListener("DOMActivate", function(e) {
+        var me = e.target;
+        var mydiv = me.parentNode;
+        var dropdown = mydiv.previousSibling;
+        var titles = me.getAttribute("X-Detected-Values");
+        var selected = dropdown.options.item(dropdown.selectedIndex).value;
 
-	    var attrs = dropdown.attributes;
-	    var newTextBox = document.createElement("input");
-	    for (var k = 0; k < attrs.length; k++) {
- 	      var attr = attrs.item(k);
-	      newTextBox.setAttribute(attr.name, attr.value);
-	    }
-	    newTextBox.setAttribute("type", "text");
-		newTextBox.value = selected;
-		dropdown.parentNode.replaceChild(newTextBox, dropdown);
+        var attrs = dropdown.attributes;
+        var newTextBox = document.createElement("input");
+        for (var k = 0; k < attrs.length; k++) {
+           var attr = attrs.item(k);
+          newTextBox.setAttribute(attr.name, attr.value);
+        }
+        newTextBox.setAttribute("type", "text");
+        newTextBox.value = selected;
+        dropdown.parentNode.replaceChild(newTextBox, dropdown);
 
-		mydiv.innerHTML = "<small>Replaced title dropdown. The old choices were:<br> " + titles + "</small>";
+        mydiv.innerHTML = "<small>Replaced title dropdown. The old choices were:<br> " + titles + "</small>";
 /*
-	  var newDiv = document.createElement("div");
-	  newDiv.appendChild(newTextBox);
-	  newDiv.appendChild(document.createElement("br"));
-	  var small = document.createElement("small");
-	  small.style.fontWeight = "bold";
-	  small.appendChild(document.createTextNode("Auto-replaced title dropdown. The old titles were:"));
-	  small.appendChild(document.createElement("br"));
-	  small.appendChild(document.createTextNode("\"" + foundtitles.join("\", \"") + "\""));
-	  newDiv.appendChild(small);
-	  dropdown.parentNode.replaceChild(newDiv, dropdown);
+      var newDiv = document.createElement("div");
+      newDiv.appendChild(newTextBox);
+      newDiv.appendChild(document.createElement("br"));
+      var small = document.createElement("small");
+      small.style.fontWeight = "bold";
+      small.appendChild(document.createTextNode("Auto-replaced title dropdown. The old titles were:"));
+      small.appendChild(document.createElement("br"));
+      small.appendChild(document.createTextNode("\"" + foundtitles.join("\", \"") + "\""));
+      newDiv.appendChild(small);
+      dropdown.parentNode.replaceChild(newDiv, dropdown);
 */
-	    e.stopPropagation();
-		e.preventDefault();
-	  }, false);
-	  newDiv.appendChild(link);
-	  dropdown.parentNode.insertBefore(newDiv, dropdown.nextSibling);
+        e.stopPropagation();
+        e.preventDefault();
+      }, false);
+      newDiv.appendChild(link);
+      dropdown.parentNode.insertBefore(newDiv, dropdown.nextSibling);
 /*
-	  
-	  var attrs = dropdown.attributes;
-	  var newTextBox = document.createElement("input");
-	  for (var k = 0; k < attrs.length; k++) {
-	    var attr = attrs.item(k);
-	    newTextBox.setAttribute(attr.name, attr.value);
-	  }
-	  newTextBox.setAttribute("type", "text");
-	  var newDiv = document.createElement("div");
-	  newDiv.appendChild(newTextBox);
-	  newDiv.appendChild(document.createElement("br"));
-	  var small = document.createElement("small");
-	  small.style.fontWeight = "bold";
-	  small.appendChild(document.createTextNode("Auto-replaced title dropdown. The old titles were:"));
-	  small.appendChild(document.createElement("br"));
-	  small.appendChild(document.createTextNode("\"" + foundtitles.join("\", \"") + "\""));
-	  newDiv.appendChild(small);
-	  dropdown.parentNode.replaceChild(newDiv, dropdown);
-*/	  
-	}
+      
+      var attrs = dropdown.attributes;
+      var newTextBox = document.createElement("input");
+      for (var k = 0; k < attrs.length; k++) {
+        var attr = attrs.item(k);
+        newTextBox.setAttribute(attr.name, attr.value);
+      }
+      newTextBox.setAttribute("type", "text");
+      var newDiv = document.createElement("div");
+      newDiv.appendChild(newTextBox);
+      newDiv.appendChild(document.createElement("br"));
+      var small = document.createElement("small");
+      small.style.fontWeight = "bold";
+      small.appendChild(document.createTextNode("Auto-replaced title dropdown. The old titles were:"));
+      small.appendChild(document.createElement("br"));
+      small.appendChild(document.createTextNode("\"" + foundtitles.join("\", \"") + "\""));
+      newDiv.appendChild(small);
+      dropdown.parentNode.replaceChild(newDiv, dropdown);
+*/      
+    }
   }
 })();
