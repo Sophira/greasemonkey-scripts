5b7d6ce6230a3644e1777a8714d868141fd6ce1c
Sophie Hamilton <git-commits@theblob.org>
2015-11-11 01:45:35 +0000

    explainxkcd: Use "Permanent link" text for number
    The previous code wouldn't work on the main page. Also, update the list
    of sites so that it works for the www subdomain too.

--- xkcd-add-link-to-explainxkcd.user.js
+++ xkcd-add-link-to-explainxkcd.user.js
@@ -3,8 +3,14 @@
 // @author      Sophie Hamilton
 // @description Adds a link to explainxkcd.com on all xkcd comic pages.
 // @namespace   http://theblob.org/
+// @include     http://xkcd.com/
+// @include     https://xkcd.com/
 // @include     http://xkcd.com/*/
 // @include     https://xkcd.com/*/
+// @include     http://www.xkcd.com/
+// @include     https://www.xkcd.com/
+// @include     http://www.xkcd.com/*/
+// @include     https://www.xkcd.com/*/
 // @version     1
 // @grant       none
 // ==/UserScript==
@@ -12,15 +18,18 @@
 (function() {
   var title = document.getElementById("ctitle");
   if (title) {
-    var results = document.location.pathname.match(/^\/(\d+)\/$/);
-    var comicnum = results[1];
-    var span = document.createElement("span");
-    span.appendChild(document.createTextNode(" ("));
-    var link = document.createElement("a");
-    link.href = "http://www.explainxkcd.com/wiki/index.php/" + comicnum;
-    link.appendChild(document.createTextNode("explain"));
-    span.appendChild(link);
-    span.appendChild(document.createTextNode(")"));
-    title.appendChild(span);
+    var origin = document.querySelector("#ctitle + .comicNav + #comic + .comicNav + br");
+    if (origin) {
+      var results = origin.nextSibling.data.match(/Permanent link to this comic: http:\/\/xkcd\.com\/(\d+)\//);
+      var comicnum = results[1];
+      var span = document.createElement("span");
+      span.appendChild(document.createTextNode(" ("));
+      var link = document.createElement("a");
+      link.href = "http://www.explainxkcd.com/wiki/index.php/" + comicnum;
+      link.appendChild(document.createTextNode("explain"));
+      span.appendChild(link);
+      span.appendChild(document.createTextNode(")"));
+      title.appendChild(span);
+    }
   }
 })();
