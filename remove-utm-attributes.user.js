4458de88d4242532e06c66459da587f8b7c8a29b
Sophie Hamilton <git-commits@theblob.org>
2017-01-02 20:01:36 +0000

    UTM Remover: Cleanup and bugfix
    Clean up the code for the UTM Remover, and fix a bug that could cause it
    to generate invalid URLs. (Actually the bug isn't completely fixed as
    &amp; entities can still confuse it; I'm going to see what I can do to
    fix that.)

--- remove-utm-attributes.user.js
+++ remove-utm-attributes.user.js
@@ -1,23 +1,24 @@
 // ==UserScript==
-// @name           Remove ?utm_* attributes
+// @name           UTM Remover: Remove ?utm_* attributes
 // @namespace      http://theblob.org/
+// @author         Sophie Hamilton
 // @description    Remove the ?utm_* attributes used by sites that employ web analytics software. In addition to replacing links on document load, this also allows a last line of defense by removing attributes from links that are clicked just before the browser tries to load them.
 // @include        *
 // ==/UserScript==
 (function() {
+  var debugme = 0;
+
   var atags = document.getElementsByTagName("a");
   for (var i = 0; i < atags.length; i++) {
     var a = atags.item(i);
-    var hval = a.getAttribute("href");
-    if (hval) {
-      var oldval = hval;
-//      var newval = hval.replace(/(\?|&(amp;)?)utm_(source|campaign|medium|content|term)=[^&#]+/g, "");
-//      if (oldval.match(/utm/)) { console.log("GM script: Looking at " + oldval); }
+    var hval = a.search;
+    if (hval && hval.match(/utm/)) {
+      var oldhref = a.href;
+      if (debugme) { console.log("UTM Remover: Looking at " + oldhref); }
       var newval = replaceUTMs(hval);
-//      var newval = hval.replace(/utm/g, "");
-      a.setAttribute("href", newval);
-      if (oldval != newval) {
-        console.log("GM Script: Cleaned UTM attributes from link: " + oldval + " --> " + newval);
+      a.search = newval;
+      if (hval != newval) {
+        console.log("UTM Remover: Cleaned UTM attributes from link: " + oldhref + " --> " + a.href);
       }
     }
   }
@@ -25,24 +26,32 @@
   window.addEventListener("DOMActivate", function(e) {
     var targ = e.target;
     if (targ.nodeName == "A") {
-      var hval = targ.getAttribute("href");
-      if (hval.match(/utm/)) {
-//        console.log("Live-replacing UTMs in " + hval);
+      var hval = targ.search;
+      if (hval && hval.match(/utm/)) {
+        var oldhref = targ.href;
+        if (debugme) { console.log("UTM Remover: Live-replacing UTMs in " + oldhref); }
         var newval = replaceUTMs(hval);
         if (hval != newval) {
-          targ.setAttribute("href", newval);
-          console.log("GM Script: Live-cleaned UTM attributes from link: " + hval + " --> " + newval);
+          targ.search = newval;
+          console.log("UTM Remover: Live-cleaned UTM attributes from link: " + oldhref + " --> " + targ.href);
         }
       }
     }
   }, false);
 
   function replaceUTMs(hval) {
-    var newval = hval.replace(/(\?|&)utm_(?:source|campaign|medium|content|term|name)=[^&#]+((?=&|$))/g, function(matched, sep, endsep) {
-//      console.log("Matched " + matched + ", sep=" + sep + ", endsep=" + endsep);
-      if (endsep == "&") { return sep; }
-                    else { return ""; }
+    var newval = hval;
+    newval = newval.replace(/^\?/, "");
+    var attribs = newval.split("&");   // FIXME: this fails when there's an &amp; in the arguments.
+    var newattribs = [];
+    attribs.forEach(function(el, i) {
+      if (!(el.match(/^utm_(?:source|campaign|medium|content|term|name)=.*$/))) {
+        newattribs.push(el);
+      }
+      else if (debugme) { console.log("UTM Remover: Matched " + el + ", removing"); }
     });
+    if (newattribs.length > 0) { newval = "?" + newattribs.join("&"); }
+                          else { newval = ""; }
     return newval;
   }
 })();
