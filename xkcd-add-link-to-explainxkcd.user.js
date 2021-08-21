00ca567796914acee04be2a7e23b75262dbbf8ab
Sophie Hamilton <git-commits@theblob.org>
2021-08-21 17:30:05 +0100

    explainxkcd: Make script work again
    This script had broken because the link to the comic was actually
    changed to be a clickable link, which the script wasn't expecting.

--- xkcd-add-link-to-explainxkcd.user.js
+++ xkcd-add-link-to-explainxkcd.user.js
@@ -11,19 +11,21 @@
 // @include     https://www.xkcd.com/
 // @include     http://www.xkcd.com/*/
 // @include     https://www.xkcd.com/*/
-// @version     3
-// @grant       none
+// @version     4
+// @grant       zzzsophzzz
 // ==/UserScript==
 
 (function() {
   var title = document.getElementById("ctitle");
   var origin = document.querySelector("#ctitle + .comicNav + #comic + .comicNav + br");
   if (title && origin) {
-    var results = origin.nextSibling.data.match(/Permanent link to this comic: https:\/\/xkcd\.com\/(\d+)\//);
-    if (results) {
-      // Add custom styling for our new "Explain" button
-      var style = document.createElement("style");
-      style.appendChild(document.createTextNode("ul.comicNav li.sph-explainbutton a {\
+    if (origin.nextSibling.data.match(/Permanent link to this comic:/)) {
+      var a = document.querySelector("#ctitle + .comicNav + #comic + .comicNav + br + a[href]")
+      var results = a.href.match(/^https:\/\/xkcd\.com\/(\d+)\/?$/);
+      if (results) {
+        // Add custom styling for our new "Explain" button
+        var style = document.createElement("style");
+        style.appendChild(document.createTextNode("ul.comicNav li.sph-explainbutton a {\
   background-color: #FF4040;\
 }\
 \
@@ -31,21 +33,22 @@ ul.comicNav li.sph-explainbutton a:hover, ul.comicNav li.sph-explainbutton a:foc
   background-color: #FFFFFF;\
   color: #FF4040;\
 }"));
-      document.head.appendChild(style);
+        document.head.appendChild(style);
 
-      // find the navigation bars - specifically the "Random" button in each
-      var comicnum = results[1];
-      var randombuttons = document.querySelectorAll("#middleContainer > ul.comicNav > li > a[href='//c.xkcd.com/random/comic/']");
-      for (var i = 0; i < randombuttons.length; i++) {
-        var li = document.createElement("li");
-        li.className = "sph-explainbutton";
-        var link = document.createElement("a");
-        link.href = "https://www.explainxkcd.com/wiki/index.php/" + comicnum;
-        link.appendChild(document.createTextNode("Explain"));
-        li.appendChild(link);
-        var randomli = randombuttons.item(i).parentNode;
-        randomli.parentNode.insertBefore(li, randomli);
-        randomli.parentNode.insertBefore(document.createTextNode("\n"), randomli);
+        // find the navigation bars - specifically the "Random" button in each
+        var comicnum = results[1];
+        var randombuttons = document.querySelectorAll("#middleContainer > ul.comicNav > li > a[href='//c.xkcd.com/random/comic/']");
+        for (var i = 0; i < randombuttons.length; i++) {
+          var li = document.createElement("li");
+          li.className = "sph-explainbutton";
+          var link = document.createElement("a");
+          link.href = "https://www.explainxkcd.com/wiki/index.php/" + comicnum;
+          link.appendChild(document.createTextNode("Explain"));
+          li.appendChild(link);
+          var randomli = randombuttons.item(i).parentNode;
+          randomli.parentNode.insertBefore(li, randomli);
+          randomli.parentNode.insertBefore(document.createTextNode("\n"), randomli);
+        }
       }
     }
   }
